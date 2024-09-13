73ef23d8ed5ded58977da76f3c5bcd81e14b317b
Willy Hardy <whardy@redhat.com>
2024-09-13 12:13:21 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,35 @@
+# Frogo
+
+Frogo is a Go-based application that generates a set of dots and applies forces to them to create a visually appealing distribution. The final output is saved as an SVG file, and a PNG conversion is also provided.
+
+## Features
+
+- Generates a specified number of dots within a defined width and height.
+- Applies forces to the dots to achieve an ideal spacing.
+- Saves the output as an SVG file.
+- Converts the SVG file to a PNG format.
+
+## Installation
+
+To use Frogo, you need to have Go installed on your machine. You can download and install Go from [here](https://golang.org/dl/).
+
+Clone the repository:
+```sh
+git clone https://github.com/wjhrdy/frogo.git
+cd frogo
+```
+
+## Usage
+
+To run Frogo, use the following command:
+```sh
+go run main.go
+```
+
+## Contributing
+
+Contributions are welcome! If you find any issues or have suggestions for improvements, please open an issue or submit a pull request.
+
+## License
+
+This project is licensed under the MIT License. See the `LICENSE` file for more details.
---
+++ main.go
@@ -0,0 +1,122 @@
+package main
+
+import (
+	"fmt"
+	"math"
+	"math/rand"
+	"os"
+	"time"
+
+	"github.com/fogleman/gg" // Replace the cairo import with this
+)
+
+const (
+	width           = 800
+	height          = 600
+	dotRadius       = 15
+	numDots         = 240
+	centerLineY     = height / 2
+	iterations      = 10000
+	avoidanceHeight = 30 // New constant for the height of the area to avoid
+)
+
+type Dot struct {
+	x, y float64
+}
+
+func main() {
+	rand.Seed(time.Now().UnixNano())
+
+	dots := make([]Dot, numDots)
+	for i := range dots {
+		dots[i] = Dot{
+			x: rand.Float64() * width,
+			y: rand.Float64() * height,
+		}
+	}
+
+	for i := 0; i < iterations; i++ {
+		applyForces(dots)
+	}
+
+	file, err := os.Create("output.svg")
+	if err != nil {
+		fmt.Println("Error creating file:", err)
+		return
+	}
+	defer file.Close()
+
+	writeSVG(file, dots)
+
+	// Add PNG conversion
+	writePNG(dots)
+}
+
+func applyForces(dots []Dot) {
+	idealSpacing := math.Sqrt((width * height) / float64(numDots))
+
+	for i := range dots {
+		dx, dy := 0.0, 0.0
+
+		for j := range dots {
+			if i != j {
+				diffX := dots[i].x - dots[j].x
+				diffY := dots[i].y - dots[j].y
+				distance := math.Sqrt(diffX*diffX + diffY*diffY)
+
+				if distance < idealSpacing {
+					force := (idealSpacing - distance) / idealSpacing
+					dx += force * diffX / distance
+					dy += force * diffY / distance
+				}
+			}
+		}
+
+		// Move dot
+		dots[i].x += dx
+		dots[i].y += dy
+
+		// Avoid center area
+		if math.Abs(dots[i].y-centerLineY) < dotRadius+avoidanceHeight/2 {
+			if dots[i].y < centerLineY {
+				dots[i].y = centerLineY - (dotRadius + avoidanceHeight/2)
+			} else {
+				dots[i].y = centerLineY + (dotRadius + avoidanceHeight/2)
+			}
+		}
+
+		// Keep within bounds
+		dots[i].x = math.Max(dotRadius, math.Min(width-dotRadius, dots[i].x))
+		dots[i].y = math.Max(dotRadius, math.Min(height-dotRadius, dots[i].y))
+	}
+}
+
+func writeSVG(file *os.File, dots []Dot) {
+	file.WriteString(fmt.Sprintf(`<svg width="%d" height="%d" xmlns="http://www.w3.org/2000/svg">`, width, height))
+	file.WriteString(`<rect width="100%" height="100%" fill="black"/>`)
+
+	// Remove the line drawing code
+
+	for _, dot := range dots {
+		file.WriteString(fmt.Sprintf(`<circle cx="%f" cy="%f" r="%d" fill="white"/>`, dot.x, dot.y, dotRadius))
+	}
+
+	file.WriteString(`</svg>`)
+}
+
+func writePNG(dots []Dot) {
+	dc := gg.NewContext(width, height)
+
+	// Set background to black
+	dc.SetRGB(0, 0, 0)
+	dc.Clear()
+
+	// Draw white dots
+	dc.SetRGB(1, 1, 1)
+	for _, dot := range dots {
+		dc.DrawCircle(dot.x, dot.y, dotRadius)
+		dc.Fill()
+	}
+
+	dc.SavePNG("output.png")
+}
